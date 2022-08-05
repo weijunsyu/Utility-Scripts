70a051ecf8d22eb446cb797608bf729073196999
WeiJun Syu <weijunsyu@gmail.com>
2022-08-05 09:24:54 -0700

    Update Licence

--- FileIterativeCopy/copy_file_iterative.py
+++ FileIterativeCopy/copy_file_iterative.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) [DATE OF PUBLICATION] [NAME OF OWNER]
+# Copyright (c) 2022 WeiJun Syu
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
